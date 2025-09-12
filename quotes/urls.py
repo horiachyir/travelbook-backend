918b32418eab3a6304704d18adcaa78b6a8dd949
horiachyir <horiachyir@gmail.com>
2025-09-12 00:25:39 +0000

    fixed 500 error

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    # Support both with and without trailing slash
     path('<uuid:booking_id>/', views.delete_quote, name='delete_quote'),
+    re_path(r'^(?P<booking_id>[0-9a-f-]+)$', views.delete_quote, name='delete_quote_no_slash'),
 ]
